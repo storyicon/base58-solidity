a28840d0251be6881df4e9afa5838b2943a20195
storyicon <yuanchao@bilibili.com>
2022-07-22 01:05:51 +0800

    docs: update package
    Signed-off-by: storyicon <yuanchao@bilibili.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,4 @@
-// require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
