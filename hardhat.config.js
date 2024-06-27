2595da9ada5ac5a9deaa383605c83e699b1fd3ac
GGos3 <dev.ggos3@gmail.com>
2024-06-27 16:15:13 +0900

    chore: update dependency and upgrade Solidity version to 0.8.20

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
     solidity: {
-        version: "0.8.7",
+        version: "0.8.20",
         settings: {
             optimizer: {
                 enabled: true,
